88f15a839c0ef48ad3afce57fe9c80ae322a2f5f
webgem-jpl <julienplefebvre@gmail.com>
2016-01-14 15:22:39 -0500

    frontend removed

--- frontend/app/assets/javascripts/spree/frontend.js
+++
@@ -1,6 +0,0 @@
-//= require bootstrap-sprockets
-//= require jquery.validate/jquery.validate.min
-//= require spree
-//= require spree/frontend/cart
-//= require spree/frontend/checkout
-//= require spree/frontend/product
